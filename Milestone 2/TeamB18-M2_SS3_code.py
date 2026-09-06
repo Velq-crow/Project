6dc0d67e52ef1f64b931f7c73f9408fb4f4f8935
Velq-crow <karnatiketan@gmail.com>
2026-09-06 22:27:27 +1000

    milestone 2 naming

---
+++ Milestone 2/TeamB18-M2_SS3_code.py
@@ -0,0 +1,136 @@
+# Subsystem 3 Eng1013
+# Created Date:6/9/26
+# Created By: Ketan
+# Version: 1.3
+
+
+from pymata4 import pymata4
+import time
+import random
+
+board = pymata4.Pymata4()
+
+# Constants
+TOP_HEIGHT = 10 #cm
+maxVehHeight = 4 #cm
+
+# Polling rate
+pollingRate = 2 #seconds
+
+#TL6
+redPin =13
+yellowPin =12
+greenPin = 11
+
+#US5
+echoPin = 3
+triggerPin = 4
+# Configuring pins
+board.set_pin_mode_sonar(triggerPin,echoPin,timeout=200000)
+board.set_pin_mode_digital_output(redPin)
+board.set_pin_mode_digital_output(yellowPin)
+board.set_pin_mode_digital_output(greenPin)
+
+time.sleep(1)
+
+def heightDiff(distance):
+    """
+    Used to minus the distance measured by the ultrasonic sensor (US5) from the TOP_HEIGHT giving the vehicle height.
+
+        Parameters:
+            distance: To be used in calculation TOP_HEIGHT - distance[0]
+
+        Returns:
+            Returns TOP_HEIGHT - distance[0]
+    """
+    return TOP_HEIGHT - distance[0]
+
+def normal_state():
+    """
+    Sets the traffic light (TL6) to red.
+
+        Parameters:
+            None
+
+        Returns:
+            function has no return
+    """
+    board.digital_pin_write(redPin,1)
+    board.digital_pin_write(greenPin,0)
+    board.digital_pin_write(yellowPin,0)
+
+def overheight_state_green():
+    """
+    Sets the traffic light (TL6) to green.
+
+        Parameters:
+            None
+
+        Returns:
+            function has no return
+    """
+    board.digital_pin_write(redPin,0)
+    board.digital_pin_write(greenPin,1)
+    time.sleep(5)
+
+def overheight_state_yellow():
+    """
+    Sets the traffic light (TL6) to yellow for the requried time 3 seconds.
+
+        Parameters:
+            None
+
+        Returns:
+            function has no return
+    """
+    board.digital_pin_write(greenPin,0)
+    board.digital_pin_write(yellowPin,1)
+    time.sleep(3)
+    board.digital_pin_write(yellowPin,0)
+    board.digital_pin_write(redPin,1)
+
+def main():
+    """
+    The main function that uses the value gained from the heightDiff function and, through if statements,
+    triggers different traffic light functions based of the height difference of the overheight vehicle.
+
+        Parameters: 
+            None
+
+        Returns:
+            function has no return 
+    """
+    lastPollTime = time.time()
+    normal_state()
+    isGreen= False
+    while True:
+        try: 
+
+            currentTime = time.time()
+            if (currentTime - lastPollTime) >= pollingRate:
+                lastPollTime = currentTime
+            
+                heightUS3 = heightDiff(board.sonar_read(triggerPin)) # height of veh from us3
+                print(f"US3 height: {heightUS3:.2f} cm")
+                while True:
+                    if heightUS3 > maxVehHeight:
+                        overheight_state_green()
+                        heightUS3 = heightDiff(board.sonar_read(triggerPin)) # height of veh from us3
+                        isGreen= True
+                    else:    
+                        if isGreen == True: 
+                            overheight_state_yellow()
+                            isGreen= False
+                            break
+                        else:
+                            break
+
+
+        except KeyboardInterrupt:
+            print("\nExiting program")
+            time.sleep(1)
+            break
+
+    board.shutdown()
+if __name__ == "__main__":
+    main()
---
+++ Milestone 2/TeamB18-M2_SS4_code.py
@@ -0,0 +1,162 @@
+# Subsystem 4 Eng1013
+# Created Date:6/9/26
+# Created By: Ketan
+# Version: 1.3
+
+
+from pymata4 import pymata4
+import time
+import random
+
+board = pymata4.Pymata4()
+
+# Constants
+TOP_HEIGHT = 10 #cm
+
+# Polling rate
+pollingRate = 2 #seconds
+
+#TL3 
+redPin = 2
+greenPin = 3
+
+#US3
+triggerPinUS3 = 9
+echoPinUS3 = 6
+
+#US4
+triggerPinUS4 = 8
+echoPinUS4 = 7
+acceptableError = 30 # percentage error when cross checking
+
+# Configuring pins for TL3
+board.set_pin_mode_digital_output(redPin)
+board.set_pin_mode_digital_output(greenPin)
+
+# Configuring pins for US3 and US4
+board.set_pin_mode_sonar(triggerPinUS3,echoPinUS3,timeout=200000)
+board.set_pin_mode_sonar(triggerPinUS4,echoPinUS4,timeout=200000)
+
+# sleep to configure pins
+time.sleep(1)
+
+def heightDiff(distance):
+    """
+    Used to minus the distance measured by the ultrasonic sensor (US5) from the TOP_HEIGHT to give actual height of the vechicle.
+
+        Parameters:
+            distance: To be used in calculation TOP_HEIGHT - distance[]
+
+        Returns:
+            Returns TOP_HEIGHT - distance[0]
+    """
+    return TOP_HEIGHT - distance[0]
+    
+def normal_state():
+    """
+    This switches the pin connected to the green LED to 1 to allow traffic to flow through to the tunnel.
+
+        Parameters:
+            None
+
+        Returns:
+            This function has no returns
+    """
+    board.digital_pin_write(redPin,0)
+    board.digital_pin_write(greenPin,1)
+
+def overheight_state():
+    """
+    This switches the pin connected to the red LED to 1 to shine red and stop the overheight vehicle from moving into the tunnel.
+
+        Parameters:
+            None
+
+        Returns:
+            This function has no returns
+    """
+    board.digital_pin_write(redPin,1)
+    board.digital_pin_write(greenPin,0)
+
+def shutdown_state():
+    """
+    Switches all traffic light pins to 0 in turn shutting down all traffic LEDs.
+
+        Parameters:
+            None
+
+        Returns:
+            This function has no returns
+    """
+    board.digital_pin_write(redPin,0)
+    board.digital_pin_write(greenPin,0)
+
+def main():
+    """ 
+    The main function, that is the central control loop that continuously polls ultrasonic sensors to measure the 
+    height of the vehicle and manage traffic light states based the vehicle's height. 
+
+        Parameters:
+            None
+
+        Returns: 
+            This function has no returns
+    """
+    while True:
+        try: 
+            #Validation loop
+            overHeightLimit = None
+            while overHeightLimit is None or overHeightLimit < 0 or overHeightLimit > TOP_HEIGHT:
+                limitInput = input("Enter the height limit (m): ").strip()
+                try:
+                    if overHeightLimit == "":
+                        print("Assigning default height")
+                        overHeightLimit = 4
+                        break
+
+                    overHeightLimit = int(limitInput)
+
+                    if overHeightLimit < 0:
+                        print("Invalid input. height limit cannot be negative.")
+                        overHeightLimit = None
+
+                    if overHeightLimit > TOP_HEIGHT:
+                        print(f"Invalid input. height limit cannot be more than {TOP_HEIGHT}.")
+                        overHeightLimit = None
+                except ValueError:
+                    print("Invalid input. Please enter a whole number.")
+
+            lastPollTime = time.time()
+            while True:
+                currentTime = time.time()
+                if (currentTime - lastPollTime) >= pollingRate:
+                    lastPollTime = currentTime
+
+                    heightUS3 = heightDiff(board.sonar_read(triggerPinUS3)) # height of veh from us3
+                    heightUS4 = heightDiff(board.sonar_read(triggerPinUS4)) # height of veh from us4
+
+                    lowerErrorBound = heightUS3*(1- acceptableError/100)
+                    upperErrorBound = heightUS3*(1+ acceptableError/100)
+
+                    print(f"US3 height: {heightUS3:.2f} cm \n US4 height: {heightUS4:.2f} cm")
+
+                    if heightUS3 >= overHeightLimit:
+                        if lowerErrorBound <= heightUS4 <= upperErrorBound:
+                            overheight_state()
+                    else:
+                            normal_state()
+                    
+                time.sleep(0.05)
+
+        except KeyboardInterrupt:
+            shutdown_state()
+            print("\nExiting program")
+            time.sleep(1)
+            break
+
+    board.shutdown()
+if __name__ == "__main__":
+    main()
+
+
+
